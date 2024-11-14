1e21a3fbb4a02a64bbf1c41a53de89f31f448e7f
Nino-OS <robotic.vb21@gmail.com>
2024-11-14 19:02:58 +0100

    v.0.3

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
-const meinCoolerButton = document.getElementById("btn");
-const meinCoolerButton2 = document.getElementById("btn2");
-const meinCoolerText = document.getElementById("txt");
-const meinCoolerTextBox = document.getElementById("tb");
-
-meinCoolerButton.addEventListener("click", function() {
-    let wert = meinCoolerTextBox.value;
-
-    if (wert === "654") {
-        wert = meinCoolerText.innerText = "Du bist cool";
-    } else if (wert === "123") {
-        wert = meinCoolerText.innerText = "Du Nicht bist cool";
-    } else {
-        wert = meinCoolerTextBox.value;
+(function () {
+  function preventDevTools() {
+    if (window.devtools.open) {
+      window.location.reload();
     }
-    
+  }
 
-    hallo(wert);
+  window.devtools = { open: false };
+  window.addEventListener("devtoolschange", preventDevTools);
+
+  (function () {
+    var devtools = window.devtools;
+    var element = new Image();
+    Object.defineProperty(element, "id", {
+      get: function () {
+        devtools.open = true;
+        window.dispatchEvent(new Event("devtoolschange"));
+      },
+    });
+    return;
+  })();
+})();
+
+document.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
 });
 
-meinCoolerButton2.addEventListener("click", function() {
-    const wert = meinCoolerTextBox.value + " ist cool";
-    hallo(wert);
+document.addEventListener("keydown", function (event) {
+  if (event.key == "F12") {
+    event.preventDefault();
+  }
+  if (
+    (event.ctrlKey || event.metaKey) &&
+    event.shiftKey &&
+    event.keyCode == "I".charCodeAt(0)
+  ) {
+    event.preventDefault();
+  }
+  if (
+    (event.ctrlKey || event.metaKey) &&
+    event.shiftKey &&
+    event.keyCode == "J".charCodeAt(0)
+  ) {
+    event.preventDefault();
+  }
+  if ((event.ctrlKey || event.metaKey) && event.keyCode == "U".charCodeAt(0)) {
+    event.preventDefault();
+  }
+  if (
+    (event.ctrlKey || event.metaKey) &&
+    event.shiftKey &&
+    event.keyCode == 67
+  ) {
+    event.preventDefault();
+  }
+  if ((event.ctrlKey || event.metaKey) && event.keyCode == 83) {
+    event.preventDefault();
+  }
 });
-
-/* setInterval(() => {
-    const wert = meinCoolerTextBox.value;
-    meinCoolerText.innerText = `Hallo, ${wert}`
-}, 6000); */
-
-function hallo(name) {
-
-    if (name === "") {
-        name = "Das Feld ist lehr!";
-
-        setTimeout(() => {
-            name = "Hi";
-
-            setTimeout(() => {
-                meinCoolerText.innerText = `${name}`;
-            }, 100);
-
-        }, 2900);
-
-
-    }
-
-    meinCoolerText.innerText = `Hallo, ${name}`;
-}
