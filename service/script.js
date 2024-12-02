7a002e9c701eac3aae44d9def50bcc29f7ae8ad2
Nino-OS <robotic.vb21@gmail.com>
2024-12-02 17:39:30 +0100

    v.3.9

--- service/script.js
+++ service/script.js
@@ -69,6 +69,19 @@ window.addEventListener("resize", () => {
 createSnowflakes();
 animateSnowfall();
 
+function scrollToElement(elementSelector, instance = 0) {
+  const elements = document.querySelectorAll(elementSelector);
+  if (elements.length > instance) {
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+    scrollToElement(".showcase");
+  }, 600);
+});
+
 
 /*   (function () {
     function preventDevTools() {
